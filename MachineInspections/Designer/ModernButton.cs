b27766825888d15fa95aeecd90241399245e8e80
pyuval <yuval.peery@hotmail.com>
2026-06-28 07:17:59 +0300

    AButt

--- MachineInspections/Designer/ModernButton.cs
+++ MachineInspections/Designer/ModernButton.cs
@@ -7,25 +7,33 @@ using System.Threading.Tasks;
 namespace MachineInspections.Designer
 {
     public class ModernButton : Button
+ public class MaterialButton : Button
     {
-        public ModernButton()
+        private Color _defaultColor = Color.White;
+        private Color _hoverColor = Color.FromArgb(240, 240, 240);
+        private Color _borderColor = Color.FromArgb(33, 150, 243); // Material Blue 500
+
+        public MaterialButton()
         {
             this.FlatStyle = FlatStyle.Flat;
-            this.FlatAppearance.BorderSize = 0;
-            this.BackColor = Color.White;
+            this.FlatAppearance.BorderSize = 2;
+            this.FlatAppearance.BorderColor = _borderColor;
+
+            this.BackColor = _defaultColor;
             this.ForeColor = Color.Black;
-            this.Font = new Font("Segoe UI", 16, FontStyle.Bold);
-            this.Height = 70;
+
+            this.Font = new Font("Segoe UI", 14, FontStyle.Bold);
+            this.Height = 80;
             this.Width = 350;
-            this.Margin = new Padding(10);
+            this.Margin = new Padding(15);
             this.Cursor = Cursors.Hand;
 
-            this.Region = System.Drawing.Region.FromHrgn(
+            this.Region = Region.FromHrgn(
                 CreateRoundRectRgn(0, 0, Width, Height, 20, 20)
             );
 
-            this.MouseEnter += (s, e) => this.BackColor = Color.FromArgb(230, 230, 230);
-            this.MouseLeave += (s, e) => this.BackColor = Color.White;
+            this.MouseEnter += (s, e) => this.BackColor = _hoverColor;
+            this.MouseLeave += (s, e) => this.BackColor = _defaultColor;
         }
 
         [System.Runtime.InteropServices.DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
@@ -36,5 +44,6 @@ namespace MachineInspections.Designer
             int nWidthEllipse, int nHeightEllipse
         );
     }
+
 
 }
